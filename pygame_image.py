973628199d86a20b5ab0031759ca06cf879723b5
yuhonma <c0a2213006@edu.teu.ac.jp>
2023-04-18 15:27:03 +0900

    練習問題2

--- pygame_image.py
+++ pygame_image.py
@@ -6,10 +6,12 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01-20230418/fig/pg_bg.jpg")
+    kk_img = pg.image.load("ex01-20230418/fig/3.png")
+    kk_img = pg.transform.flip(kk_img, True, False)
+    
+
 
     tmr = 0
-
-
 
     while True:
         for event in pg.event.get():
